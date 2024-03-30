79ef30f6730501d9811adc28f612fc80b6067d5e
Sebastian Peña <sebastianpmtz@gmail.com>
2024-03-30 14:38:57 -0600

    Changes in directorys and add new project.

---
+++ client_contact/src/test/java/com/microservices/client_contact/ClientContactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.client_contact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientContactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
